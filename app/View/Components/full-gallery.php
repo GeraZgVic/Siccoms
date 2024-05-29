e237c2c3207db06b7419e91b60cfe9afff8204c5
Victor Zúñiga <gerazgvic@gmail.com>
2024-05-28 19:13:02 -0600

    Cambio de nombre para evitar de error por el gion

--- app/View/Components/full-gallery.php
+++ app/View/Components/full-gallery.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class full-gallery extends Component
+class Fullgallery extends Component
 {
     /**
      * Create a new component instance.
