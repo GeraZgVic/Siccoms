55916397622a508017bbe7ec1619490149680f36
Victor Zúñiga <gerazgvic@gmail.com>
2024-06-10 11:58:41 -0600

    Tipografía cambiada en el componente

--- resources/views/livewire/text-animation.blade.php
+++ resources/views/livewire/text-animation.blade.php
@@ -32,6 +32,6 @@ gsapInterval2 = setInterval(function() {
         clearInterval(gsapInterval2);
     }
 }, 5);"
-    class="invisible block pb-4 overflow-hidden text-4xl md:text-5xl lg:text-7xl mb-2 md:m-0 font-bold custom-font tracking-tight text-gray-800">
+    class="invisible block pb-4 overflow-hidden text-4xl md:text-5xl lg:text-7xl mb-2 md:m-0 font-bold custom-font tracking-tight text-gray-800 font-mulish-bold">
     {{$texto}}
 </h1>
