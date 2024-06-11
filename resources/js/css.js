fec199b76edd9c5db0ff4fdf5084ed1d860f013b
Victor Zúñiga <gerazgvic@gmail.com>
2024-06-10 19:11:23 -0600

    Animaciones agregadas, efecto bottom,left,right

--- resources/js/css.js
+++ resources/js/css.js
@@ -10,27 +10,66 @@ function iniciarApp() {
 
 function aplicarAnimaciones() {
     let secciones = document.querySelectorAll(".seccion");
+    let animate_right = document.querySelectorAll(".animate-right");
+    let animate_left = document.querySelectorAll(".animate-left");
+    let animate_bottom = document.querySelectorAll(".animate-bottom");
 
     function mostrarAnimaciones() {
         let scroll = window.scrollY;
         let windowHeight = window.innerHeight;
 
-        secciones.forEach(function (seccion) {
-            let offsetTop = seccion.offsetTop;
-            let seccionHeight = seccion.offsetHeight;
+        // Aplica animaciones a las secciones
+        if (secciones.length > 0) {
+            secciones.forEach(function (seccion) {
+                let offsetTop = seccion.offsetTop;
+                let seccionHeight = seccion.offsetHeight;
 
-            if (scroll + windowHeight >= offsetTop && scroll <= offsetTop + seccionHeight) {
-                seccion.classList.add("animate-fade", "animate-once", "animate-duration-[600ms]", "animate-delay-200", "animate-ease-in", "animate-normal");
-            }
-        });
+                if (scroll + windowHeight >= offsetTop && scroll <= offsetTop + seccionHeight) {
+                    seccion.classList.add("animate-fade", "animate-once", "animate-duration-[600ms]", "animate-delay-200", "animate-ease-in", "animate-normal");
+                }
+            });
+        }
+
+        // Izquierda a derecha
+        if (animate_right.length > 0) {
+            animate_right.forEach(function (animate) {
+                let offsetTop = animate.offsetTop;
+                let animateHeight = animate.offsetHeight;
+
+                if (scroll + windowHeight >= offsetTop && scroll <= offsetTop + animateHeight) {
+                    animate.classList.add("animate-fade-right", "animate-ease-in-out");
+                }
+            });
+        }
+
+        // Derecha a izquierda
+        if (animate_left.length > 0) {
+            animate_left.forEach(function (animate) {
+                let offsetTop = animate.offsetTop;
+                let animateHeight = animate.offsetHeight;
+
+                if (scroll + windowHeight >= offsetTop && scroll <= offsetTop + animateHeight) {
+                    animate.classList.add("animate-fade-left", "animate-ease-in-out");
+                }
+            });
+        }
+
+        // De arriba hacia abajo
+        if (animate_bottom.length > 0) {
+            animate_bottom.forEach(function (animate) {
+                let offsetTop = animate.offsetTop;
+                let animateHeight = animate.offsetHeight;
+
+                if (scroll + windowHeight >= offsetTop && scroll <= offsetTop + animateHeight) {
+                    animate.classList.add("animate-fade-down", "animate-ease-in-out");
+                }
+            });
+        }
     }
 
-    window.addEventListener("scroll", function () {
-        mostrarAnimaciones();
-    });
+    // Añade un event listener para el scroll
+    window.addEventListener("scroll", mostrarAnimaciones);
 
     // Llama a mostrarAnimaciones() para aplicar animaciones cuando la página se carga inicialmente
     mostrarAnimaciones();
 }
-
-
