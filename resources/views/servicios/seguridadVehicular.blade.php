5dd71804363cd42a4220af63dbdd10f3f5f94108
Carlo Reyes <carlosrey1910@gmail.com>
2024-06-12 14:13:19 -0600

    Cambio de movimiento imagen heros

--- resources/views/servicios/seguridadVehicular.blade.php
+++ resources/views/servicios/seguridadVehicular.blade.php
@@ -10,9 +10,11 @@
 <div class="grid grid-cols-1 md:grid-cols-2 gap-8 px-6 py-12 h-[100vh]">
     <div>
         <section>
-            <h1 class="pb-4 text-3xl md:text-4xl lg:text-6xl mb-2 md:m-0 font-mulish-bold text-gray-800 animate-top"> Control de Acceso
+            <h1 class="pb-4 text-3xl md:text-4xl lg:text-6xl mb-2 md:m-0 font-mulish-bold text-gray-800 animate-top">
+                Control de Acceso
                 Vehicular</h1>
-            <h2 class="text-xl font-mulish-bold mb-4 text-gray-700 animate-top">En SICCOMS, ofrecemos soluciones de control de acceso
+            <h2 class="text-xl font-mulish-bold mb-4 text-gray-700 animate-top">En SICCOMS, ofrecemos soluciones de
+                control de acceso
                 vehicular para mejorar la seguridad y eficiencia en cualquier entorno.
             </h2>
             <ul class="list-none mb-6 space-y-3 animate-bottom">
@@ -65,16 +67,20 @@
         </section>
     </div>
     <div>
-        <img src="{{ asset('img/hero-vehicular1.jpg') }}" alt="Barrera vehicular" class="w-full h-auto rounded-lg animate-left animate-duration-[2000ms]">
+        <img src="{{ asset('img/hero-vehicular1.jpg') }}" alt="Barrera vehicular"
+            class="w-full h-auto rounded-lg animate-right animate-duration-[2000ms]">
     </div>
 </div>
 
 <section class="font-inter antialiased bg-cover bg-center bg-blend-overlay bg-fixed bg-black/50"
     style="background-image: url('/img/content-vehicular1.jpg')">
     <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-        <h1 class="mb-4 text-2xl font-mulish-bold tracking-tight leading-none text-white md:text-3xl lg:text-5xl animate-top">¿Por Qué
+        <h1
+            class="mb-4 text-2xl font-mulish-bold tracking-tight leading-none text-white md:text-3xl lg:text-5xl animate-top">
+            ¿Por Qué
             Elegir Nuestras Barreras Vehiculares?</h1>
-        <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48 animate-bottom">Nuestras barreras vehiculares
+        <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48 animate-bottom">Nuestras barreras
+            vehiculares
             ofrecen tecnología avanzada, seguridad robusta y fácil integración para satisfacer tus necesidades de
             control de acceso vehicular.</p>
     </div>
@@ -85,7 +91,8 @@
         <div class="font-light text-gray-600 sm:text-lg">
             <h2 class="mb-4 text-2xl md:text-4xl tracking-tight font-mulish-bold text-gray-900 animate-top">
                 Beneficios Clave</h2>
-            <p class="mb-4 animate-top">Nuestros servicios ofrecen una mayor seguridad, eficiencia en el control del tráfico,
+            <p class="mb-4 animate-top">Nuestros servicios ofrecen una mayor seguridad, eficiencia en el control del
+                tráfico,
                 reducción de costos operativos y tranquilidad para ti y tu entorno.</p>
             <ul class="list-none mb-6 space-y-3">
                 <li class="flex items-center mb-2">
@@ -97,7 +104,9 @@
                         <path d="M12 11m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
                         <path d="M12 12l0 2.5" />
                     </svg>
-                    <span class="text-gray-700 tracking-wide font-light capitalize animate-right animate-duration-[2000ms]">Aumenta la Seguridad</span>
+                    <span
+                        class="text-gray-700 tracking-wide font-light capitalize animate-right animate-duration-[2000ms]">Aumenta
+                        la Seguridad</span>
                 </li>
                 <li class="flex items-center mb-2">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-car mr-2" width="24"
@@ -108,7 +117,8 @@
                         <path d="M17 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
                         <path d="M5 17h-2v-6l2 -5h9l4 5h1a2 2 0 0 1 2 2v4h-2m-4 0h-6m-6 -6h15m-6 0v-5" />
                     </svg>
-                    <span class="text-gray-700 tracking-wide font-light animate-right animate-duration-[2000ms]">Mejora la Gestión del Tráfico</span>
+                    <span class="text-gray-700 tracking-wide font-light animate-right animate-duration-[2000ms]">Mejora
+                        la Gestión del Tráfico</span>
                 </li>
                 <li class="flex items-center mb-2">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mood-check mr-2"
@@ -121,7 +131,9 @@
                         <path d="M9.5 15c.658 .64 1.56 1 2.5 1s1.842 -.36 2.5 -1" />
                         <path d="M15 19l2 2l4 -4" />
                     </svg>
-                    <span class="text-gray-700 tracking-wide font-light animate-right animate-duration-[2000ms]">Reducción de Costos Operativos</span>
+                    <span
+                        class="text-gray-700 tracking-wide font-light animate-right animate-duration-[2000ms]">Reducción
+                        de Costos Operativos</span>
                 </li>
                 <li class="flex items-center mb-2">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-bell-ringing mr-2"
@@ -134,14 +146,17 @@
                         <path d="M21 6.727a11.05 11.05 0 0 0 -2.794 -3.727" />
                         <path d="M3 6.727a11.05 11.05 0 0 1 2.792 -3.727" />
                     </svg>
-                    <span class="text-gray-700 tracking-wide font-light animate-right animate-duration-[2000ms]">Confianza y Tranquilidad</span>
+                    <span
+                        class="text-gray-700 tracking-wide font-light animate-right animate-duration-[2000ms]">Confianza
+                        y Tranquilidad</span>
                 </li>
             </ul>
         </div>
         <div class="grid grid-cols-2 gap-4 mt-8">
-            <img class="w-full rounded-lg animate-bottom animate-duration-[2000ms]" src="{{asset('img/content-vehicular3.jpg')}}" alt="Barreras vehiculares">
-            <img class="mt-4 w-full lg:mt-10 rounded-lg animate-top animate-duration-[2000ms]" src="{{asset('img/content-vehicular4.jpg')}}"
-                alt="Barreras vehiculares">
+            <img class="w-full rounded-lg animate-bottom animate-duration-[2000ms]"
+                src="{{asset('img/content-vehicular3.jpg')}}" alt="Barreras vehiculares">
+            <img class="mt-4 w-full lg:mt-10 rounded-lg animate-top animate-duration-[2000ms]"
+                src="{{asset('img/content-vehicular4.jpg')}}" alt="Barreras vehiculares">
         </div>
     </div>
 </section>
@@ -152,7 +167,8 @@
             class="w-full h-full object-cover rounded-lg shadow-md animate-left animate-duration-[3000ms]">
     </div>
     <div class="md:w-1/2 px-6 py-16">
-        <h2 class="mb-4 text-2xl md:text-4xl tracking-tight font-mulish-bold text-gray-900 animate-top">¿Quiénes Se Benefician de
+        <h2 class="mb-4 text-2xl md:text-4xl tracking-tight font-mulish-bold text-gray-900 animate-top">¿Quiénes Se
+            Benefician de
             Nuestro Servicio?</h2>
         <h2 class="text-lg font-mulish-bold text-gray-700 mb-6 animate-bottom">En
             <span class="font-bold text-gray-900">SICCOMS</span> Somos reconocidos como los mejores instaladores de
@@ -172,7 +188,8 @@
                     <path d="M9 15l0 .01" />
                     <path d="M9 18l0 .01" />
                 </svg>
-                <span class="text-gray-700 tracking-wide font-light animate-right animate-duration-[2000ms]">Empresas y Corporaciones</span>
+                <span class="text-gray-700 tracking-wide font-light animate-right animate-duration-[2000ms]">Empresas y
+                    Corporaciones</span>
             </li>
             <li class="flex items-center mb-2">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-building-community mr-2"
@@ -185,7 +202,8 @@
                     <path d="M17 11l0 .01" />
                     <path d="M17 15l0 .01" />
                 </svg>
-                <span class="text-gray-700 tracking-wide font-light animate-right animate-duration-[2000ms]">Comunidades Residenciales</span>
+                <span class="text-gray-700 tracking-wide font-light animate-right animate-duration-[2000ms]">Comunidades
+                    Residenciales</span>
             </li>
             <li class="flex items-center mb-2">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-building-store mr-2"
@@ -198,7 +216,8 @@
                     <path d="M19 21l0 -10.15" />
                     <path d="M9 21v-4a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v4" />
                 </svg>
-                <span class="text-gray-700 tracking-wide font-light animate-right animate-duration-[2000ms]">Centros Comerciales</span>
+                <span class="text-gray-700 tracking-wide font-light animate-right animate-duration-[2000ms]">Centros
+                    Comerciales</span>
             </li>
             <li class="flex items-center mb-2">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-building-factory-2 mr-2"
@@ -212,7 +231,9 @@
                     <path d="M9 17h1" />
                     <path d="M14 17h1" />
                 </svg>
-                <span class="text-gray-700 tracking-wide font-light animate-right animate-duration-[2000ms]">Instalaciones Industriales</span>
+                <span
+                    class="text-gray-700 tracking-wide font-light animate-right animate-duration-[2000ms]">Instalaciones
+                    Industriales</span>
             </li>
         </ul>
     </div>
