36f04e4d38a698a562bc1a2280e19714afb9985d
Victor Zúñiga <gerazgvic@gmail.com>
2024-06-12 13:43:06 -0600

    Entrada de blog agregada usando la base de datos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Posts;
+
 // use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index() {
-        
-        return view('home');
+        $posts = Posts::paginate(3);
+        // Obtener el primer post y eliminarlo de la colleción
+        $firstPost = $posts->shift();
+
+        return view('home', [
+            'firstPost' => $firstPost,
+            'posts' => $posts
+        ]);
     }
 }
